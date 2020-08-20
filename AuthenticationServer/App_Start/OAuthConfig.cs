2d451894945892be27f2d72a91fb83a6c24dcff6
Óscar Rupérez <oscar.ruperez@sngular.com>
2020-08-20 12:18:52 +0200

    Configuración de un servidor de autenticación

---
+++ AuthenticationServer/App_Start/OAuthConfig.cs
@@ -0,0 +1,30 @@
+﻿using AuthenticationServer.Providers;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AuthenticationServer.App_Start
+{
+    public static class OAuthConfig
+    {
+        public static void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider()
+            };
+
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+        }
+    }
+}
---
+++ AuthenticationServer/Startup.cs
@@ -0,0 +1,25 @@
+﻿using AuthenticationServer.App_Start;
+using Microsoft.Owin.Cors;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace AuthenticationServer
+{
+    public class Startup
+    {
+
+        public void Configuration(IAppBuilder app)
+        {
+            OAuthConfig.ConfigureOAuth(app);
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseCors(CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+
+    }
+}
